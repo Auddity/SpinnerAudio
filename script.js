9c4bd8af0dee58b591764374beb79363228d9ad8
Auddity <audsecondarylap@outlook.com>
2021-08-04 16:10:11 -0400

    update

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-const date = document.getElementById('date');
-const currentYear = new Date().getFullYear();
-date.innerText = currentYear;
-
-
+// NavMenu Toggle (mobile)
 const navToggle = document.getElementById('nav-toggle');
 const linksContainer = document.getElementById('nav-links-container');
 const links = document.getElementById('links')
 
+const containerHeight = linksContainer.getBoundingClientRect().height;
 navToggle.addEventListener('click', () => {
-  const containerHeight = linksContainer.getBoundingClientRect().height;
   const linksHeight = links.getBoundingClientRect().height;
 
   if(containerHeight === 0){
@@ -18,14 +14,52 @@ navToggle.addEventListener('click', () => {
   }
 });
 
+// Back to top Link
 const navbar = document.getElementById('nav');
 const topLink = document.querySelector('.top-link');
+const navHeight = navbar.getBoundingClientRect().height;
 window.addEventListener('scroll', () => {
   const scrollHeight = window.pageYOffset;
-  const navHeight = navbar.getBoundingClientRect().height;
 
   scrollHeight > navHeight ? navbar.classList.add('fixed-nav') : navbar.classList.remove('fixed-nav');
 
   scrollHeight > 500 ? topLink.classList.add('show-link') :
   topLink.classList.remove('show-link');
 });
+
+// adjust scroll for nav height
+const linkItem = document.querySelectorAll('.scroll-link');
+linkItem.forEach(item => {
+  item.addEventListener('click', e => {
+    e.preventDefault();
+    
+    const id = e.target.getAttribute('href').slice(1);
+    const element = document.getElementById(id);
+    const fixedNav = navbar.classList.contains('fixed-nav');
+    let position = element.offsetTop - navHeight;
+    if(!fixedNav) position = position + navHeight;
+    if(navHeight > 103) position = position + containerHeight;
+    window.scrollTo(
+      { left: 0, top: position }
+    )
+
+    linksContainer.style.height = 0;
+      
+    // Show current section highlight in navbar
+    linkItem.forEach(link => {
+      if(link.classList.contains('current')) link.classList.remove('current');
+    });
+    e.currentTarget.classList.add('current');  
+
+    topLink.addEventListener('click', () => {
+      window.scrollTo(
+        { left: 0, top: position }
+      )
+    });
+  });
+});
+
+// Date
+const date = document.getElementById('date');
+const currentYear = new Date().getFullYear();
+date.innerText = currentYear;
