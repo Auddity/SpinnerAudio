656b9024a368f81479b9a8bd44d52a5fb741ac1e
Auddity <audsecondarylap@outlook.com>
2021-08-22 15:38:50 -0400

    fixed scroll from landing

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // NavMenu Toggle (mobile)
 const navToggle = document.getElementById('nav-toggle');
 const linksContainer = document.getElementById('nav-links-container');
-const links = document.getElementById('links')
+const links = document.getElementById('links');
 
 navToggle.addEventListener('click', () => {
   const containerHeight = linksContainer.getBoundingClientRect().height;
@@ -14,52 +14,89 @@ navToggle.addEventListener('click', () => {
   }
 });
 
-// Back to top Link
+// Fixed Nav & Top Link
 const navbar = document.getElementById('nav');
 const topLink = document.querySelector('.top-link');
-const navHeight = navbar.getBoundingClientRect().height;
 window.addEventListener('scroll', () => {
   const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
 
   scrollHeight > navHeight ? navbar.classList.add('fixed-nav') : navbar.classList.remove('fixed-nav');
 
-  scrollHeight > 500 ? topLink.classList.add('show-link') :
+  scrollHeight > 400 ? topLink.classList.add('show-link') :
   topLink.classList.remove('show-link');
 });
 
 // adjust scroll for nav height
-const linkItem = document.querySelectorAll('.scroll-link');
-linkItem.forEach(item => {
-  item.addEventListener('click', e => {
-    e.preventDefault();
+// const linkItem = document.querySelectorAll('.scroll-link');
+// linkItem.forEach(item => {
+//   item.addEventListener('click', e => {
+//     e.preventDefault();
+
+//     const containerHeight = linksContainer.getBoundingClientRect().height;
+//     const linksHeight = links.getBoundingClientRect().height; 
     
-    const id = e.target.getAttribute('href').slice(1);
+//     const id = e.currentTarget.getAttribute('href').slice(1);
+//     const element = document.getElementById(id);
+//     const fixedNav = navbar.classList.contains('fixed-nav');
+//     let position = element.offsetTop - linksHeight;
+
+//     if(!fixedNav) position = position - linksHeight;
+//     if(linksHeight > 103) position = position + containerHeight;
+//     window.scrollTo(
+//       { left: 0, top: position }
+//     )
+
+//     linksContainer.style.height = 0;
+      
+//     // Show current section highlight in navbar
+//     linkItem.forEach(link => {
+//       if(link.classList.contains('current')) link.classList.remove('current');
+//     });
+//     e.currentTarget.classList.add('current');  
+
+//     topLink.addEventListener('click', () => {
+//       window.scrollTo(
+//         { left: 0, top: position }
+//       )
+//     });
+//   });
+// });
+
+const scrollLinks = document.querySelectorAll('.scroll-link');
+
+scrollLinks.forEach( link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+
+    const id = e.currentTarget.getAttribute('href').slice(1);
     const element = document.getElementById(id);
+
+    console.log(element.offsetTop)
+
     const fixedNav = navbar.classList.contains('fixed-nav');
     let position = element.offsetTop - navHeight;
-    if(!fixedNav) position = position + navHeight;
-    if(navHeight > 103) position = position + containerHeight;
-    window.scrollTo(
-      { left: 0, top: position }
-    )
+    
+    if(!fixedNav) {
+      position = position - navHeight;
+    }
 
+    if(navHeight > 104) {
+      position = position + containerHeight;
+    }
+
+
+    window.scrollTo({
+      left: 0, top: position,
+    });
     linksContainer.style.height = 0;
-      
-    // Show current section highlight in navbar
-    linkItem.forEach(link => {
-      if(link.classList.contains('current')) link.classList.remove('current');
-    });
-    e.currentTarget.classList.add('current');  
-
-    topLink.addEventListener('click', () => {
-      window.scrollTo(
-        { left: 0, top: position }
-      )
-    });
+    
   });
 });
 
 // Date
 const date = document.getElementById('date');
-const currentYear = new Date().getFullYear();
-date.innerText = currentYear;
+date.innerText = new Date().getFullYear();
